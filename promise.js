3cb5ea728c5eb421a988e62d236ea340afd3fb53
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2022-09-18 09:42:26 +0700

    fix file promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let counter = 0;
+      const data = [...await promiseTheaterVGC(), ...await promiseTheaterIXX()];
+      data.forEach(d => {
+        if (d.hasil === emosi) counter++;
+      });
+      resolve(counter);
+    } catch (err) {
+      reject(err);
+    }
+  })
+};
 
 module.exports = {
   promiseOutput,
